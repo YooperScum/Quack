aa9d3739949c07a8d9b0625c25e9f7ff3159745d
Eric Poppleton <epoppleton@student.neumont.edu>
2021-03-03 19:31:30 -0700

    Add third person camera controls

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    [SerializeField] GameObject follow;
+
+    private float xRot;
+    private float yRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(follow.transform.position.x, this.transform.position.y, follow.transform.position.z);
+
+        xRot += Input.GetAxis("CameraY");
+        yRot += Input.GetAxis("CameraX");
+
+        xRot = Mathf.Clamp(xRot, -45f, 45f);
+
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0.0f);
+    }
+}
